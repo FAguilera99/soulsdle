4a6d6eb6737a8642e19cbfba5f18231d5edb6a35
FAguilera99 <faguileralmagro@gmail.com>
2024-06-16 10:48:16 +0300

    yeag

--- src/game.js
+++ src/game.js
@@ -1,31 +1,41 @@
 "use strict";
 
-//const { i } = require("vite/dist/node/types.d-aGj9QkWt");
-
-var hp = 10;
-
 // API
 const apiUrl = "https://jgalat.github.io/ds-weapons-api/";
 const numWeapons = 116;
 
-if (sessionStorage.length == 0) {
-  const weaponId = Math.floor(Math.random() * numWeapons);
-  console.log(weaponId);
-  sessionStorage.setItem("weaponId", weaponId);
+// Main
+function main(data) {
+  var weaponId = -1;
+
+  if (sessionStorage.length == 0) {
+    weaponId = Math.floor(Math.random() * numWeapons);
+    console.log(weaponId);
+    sessionStorage.setItem("weaponId", weaponId);
+  }
+  var allWeapons = data;
+  console.log(allWeapons[0].name);
+
+  populateDropdown(allWeapons);
+
+  document.querySelectorAll(".selection").forEach((element) => {
+    element.addEventListener("click", (e) => {
+      attemptCounter();
+      selectValue(e, data);
+    });
+  });
 }
 
+//
+// Fetch weapons from API
 fetch(apiUrl)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-    populateDropdown(data);
-    document
-      .querySelectorAll(".selection")
-      .forEach(element => {
-        element.addEventListener("click", (e) => selectValue(e, data));
-      })
+    main(data);
   });
 
+
+//
 // Populate dropdown
 function populateDropdown(allWeapons) {
   var dropdownHtml = "";
@@ -37,9 +47,11 @@ function populateDropdown(allWeapons) {
       '" title="' +
       allWeapons[i].name +
       '"/>';
-    
+
     var finalString =
-      '<a href="#base" class="inline-block hover:bg-white pt-[5px] selection" value="' + i + '">';
+      '<a href="#" class="inline-block bg-blue-500 pt-[5px] selection" value="' +
+      i +
+      '">';
     finalString += imgString;
     finalString +=
       '<span title="' +
@@ -48,6 +60,7 @@ function populateDropdown(allWeapons) {
       allWeapons[i].name +
       "</span>";
     finalString += "</a>";
+    //console.log(finalString);
 
     const template = document.createElement("template");
     template.innerHTML = finalString;
@@ -68,13 +81,15 @@ document.querySelector("#searchInput").addEventListener("input", searchFilter);
 
 // Close and open dropdown
 document.addEventListener("click", (e) => {
-  if (!e.target.closest("#dropdownProfileButton")) {
+  if (!e.target.closest("#searchInput")) {
     document.querySelector("#myDropdown").classList.add("hidden");
   }
 });
 
 document.querySelector("#searchInput").addEventListener("click", (e) => {
+  
   if (e.target.value != "") {
+    console.log("boop");
     document.querySelector("#myDropdown").classList.remove("hidden");
   }
 });
@@ -98,18 +113,31 @@ function searchFilter() {
 
 // Select value in Search
 function selectValue(e, allWeapons) {
-  console.log(e.target.title);
+  //console.log(e.target.title);
   document.querySelector("#searchInput").value = e.target.title;
 }
 
 // Guesses boxes
-document.querySelector("#btnGuess").addEventListener("click", (e) => {
-  var searchInput = document.querySelector("#searchInput");
-  if (searchInput.value != null) console.log("");
-});
+// document.querySelector("#btnGuess").addEventListener("click", (e) => {
+// var searchInput = document.querySelector("#searchInput");
+//  if (searchInput.value != null) console.log("");
+//});
 
+// Counter
+var attempts = 10;
+function attemptCounter(){
+  attempts -= 1;
+  document.querySelector("#counter").innerHTML = attempts;
+}
+
+// Insert boxes when guessing
 function insertGuessRow() {
   var div = document.querySelector("#guessesDiv");
 
   div.appendChild(buildGuessBox());
 }
+
+// Clue button
+document.querySelector("#counter").addEventListener("change", e => {
+  console.log(e.target.value);
+})
