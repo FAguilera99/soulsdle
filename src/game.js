0aec81fd04fe1563987564227fb4b89d41be9438
francisco <francisco@timbererp.com>
2024-06-18 11:15:47 +0300

    theming in progress

--- src/game.js
+++ src/game.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import 'animate.css';
+
 // API
 const apiUrl = "https://jgalat.github.io/ds-weapons-api/";
 const numWeapons = 116;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./*.html",
-    "./src/**/*.{html,js}"
-  ],
+  content: ["./*.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#1B1A55",
 
+          secondary: "#535C91",
+
+          accent: "#6366f1",
+
+          neutral: "#f3f4f6",
+
+          "base-100": "#070F2B",
+
+          info: "#0000ff",
+
+          success: "#16a34a",
+
+          warning: "#2563eb",
+
+          error: "#b91c1c",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
